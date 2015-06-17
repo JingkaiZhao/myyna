c062dd89345711ffb6bebdc034503b2743a4e5aa
JingkaiZhao <jingkai.zhao@sap.com>
2015-06-17 14:26:23 +0800

    sample single page finish

--- src/PageFliper.js
+++ src/PageFliper.js
@@ -1,11 +1,14 @@
 var SinglePage = React.createClass({
     render: function() {
+        var pageNum = this.props.pageNum;
+        var pageId = "item" + pageNum;
+        var imageSrc = this.props.imgSsrc;
         return (
-            <div className="bb-item" id="item0">
+            <div className="bb-item" id={pageId}>
                 <div className="content">
                     <div className="scrollert" id="nothing">
 
-                        <img src="loginpage/cubet_login1.jpg" className="image1" />
+                        <img src={imageSrc} className="image1" />
                         <div className="row image ipad">
                             <div className="col-sm-3 col-xs-offset-1">
                                 <div className="pin1">
